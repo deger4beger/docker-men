419bbf97bcc3464f2cb2e5a56239a02a73a8fe4d
Author Name <deger.begerrr@gmail.com>
2021-11-09 01:15:32 +0600

    docker compose base setup

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM node:15
 WORKDIR /app
 COPY package.json .
-RUN yarn install
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+	then yarn install; \
+	else yarn install --production=true; \
+	fi
 COPY . ./
 ENV PORT 3030
 EXPOSE $PORT
-CMD ["yarn", "dev"]
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
-const port = process.env.PORT || 3030
+
+mongoose
+	.connect("mongodb://degerbeger:mypassword@mongo:27017/?authSource=admin")
+	.then(() => console.log("Mongo is working"))
+	.catch((e) => console.log(e))
+
+
 
 app.get("/", (req, res) => {
 	return res.send("<h2>Docker hello</h2>")
 })
 
+
+const port = process.env.PORT || 3030
 app.listen(port, () => console.log(`App running on port ${port}`))
+
+
+
 
 // docker commands:
 
-// docker build -t <name> .(<path to dockerfile, "." if here>)
-// docker run -v $cd$:/app:ro(winDir:dockerDir)(for copy files - hmr)(ro - read only)
-	// -v /app/node_modules (if node modules deleted)
-	// --env-file ./.env
-	// -p 3030:3030(winPort:appPort) -d --name <name of container> <name of image>
-// docker rm docker-test-cont -f (delete container) (-fv if want to delete volumes as well)
-// docker image ls (list of images)
-// docker <image|volume> prune (delete all dangling/itermediate images (none:none))
-// docker ps (list of containers) <-a> (for all containers, stopped or crashed)
-// docker logs <name of container>
-// docker exec -it <name of container> bash
-// for bash:
-	// exit (from bash)
-	// ls (list of files in bash)
-	// cat <file name> (show file content)
-	// touch / rm <filename>
-	// printenv
+// base commands:
+	// docker build -t <name> .(<path to dockerfile, "." if here>)
+	// docker run -v $cd$:/app:ro(winDir:dockerDir)(for copy files - hmr)(ro - read only)
+		// -v /app/node_modules (if node modules deleted)
+		// --env-file ./.env
+		// -p 3030:3030(winPort:appPort) -d --name <name of container> <name of image>
+	// docker rm docker-test-cont -f (delete container) (-fv if want to delete volumes as well)
+
+// useful:
+	// docker <image|volume|network> ls (list of images)
+	// docker ps (list of containers) <-a> (for all containers, stopped or crashed) (docker-compose ps)
+	// docker <image|volume> prune (delete all dangling/itermediate images (none:none))
+	// docker logs <name of container>
+	// docker <network> inspect <container name>
+	// docker exec -it <name of container> bash (insted of bash(example): mongo -u <user> -p <password>)
+
+	// for bash:
+		// exit (from bash)
+		// ls (list of files in bash)
+		// cat <file name> (show file content)
+		// touch / rm <filename>
+		// printenv
+		// ping <other container>
+	// for compose:
+		// docker-compose up -d (--build)
+		// docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+		// docker-compose down -v (volumes down)
